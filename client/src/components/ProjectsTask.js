f9c3447f5e72e34fe008cc40afbbd1b991a81881
julian <jurianu1214@gmail.com>
2022-09-08 17:48:47 +0200

    pmv v1 100% despliegue en git sin Backend

--- client/src/components/ProjectsTask.js
+++ client/src/components/ProjectsTask.js
@@ -1,6 +1,10 @@
 // import cuberImg from "../assets/images/CUBER.jpg";
 import React, { useState,useEffect} from "react";
+import jaga from "../assets/images/project-jaga.png"
 import cuber from "../assets/images/CUBER.jpg"
+import dober from "../assets/images/doberman.jpg"
+import enmacipatic from "../assets/images/redEnmacipatic.png"
+
 import loading from "../assets/images/loading-25.gif"
 
 function ProjecstTask() {
@@ -21,24 +25,75 @@ function ProjecstTask() {
 
   return (
     <div className="taskCont">
-      {taskDates ? taskDates.map((date,i)=>
+      
       <div className="taskDiv">
          <div className="taskProject">
         
         <div className="producto_img">
-        <img className="imgCards" src={require(`../assets/images/` + date.img)} />
+        <img className="imgCards" src={jaga} />
           
         </div>
-        <h1 className="taskTitle">{date.tittle}</h1>
+        <h1 className="taskTitle">TPV Project-Jaga</h1>
         <div className="taskLine"></div>
-        <p className="tasktText">{date.Description}</p>
+        <p className="tasktText">Proyecto enfocado en la toma de pedidos y registro de actividad de empleados.Con uso de HTML, CSS ,JavScript, SesionStorage y LocalStorage</p>
         <button className="btnTask" >Vista en progreso
         <img className="loadImg" src={loading} alt="" />
         </button>
 
       </div>
       </div>
-      ) : ""}
+
+      <div className="taskDiv">
+         <div className="taskProject">
+        
+        <div className="producto_img">
+        <img className="imgCards" src={cuber} />
+          
+        </div>
+        <h1 className="taskTitle">CUBER</h1>
+        <div className="taskLine"></div>
+        <p className="tasktText">web enfocada en pedidos y geolocalización de transportes similares a empresas como UBER Y Bolt, enfocada a full Back-end, con uso de PUG, Express js, MongoDB y Mysql</p>
+        <button className="btnTask" >Vista en progreso
+        <img className="loadImg" src={loading} alt="" />
+        </button>
+
+      </div>
+      </div>
+
+      <div className="taskDiv">
+         <div className="taskProject">
+        
+        <div className="producto_img">
+        <img className="imgCards" src={dober} />
+          
+        </div>
+        <h1 className="taskTitle">DOBERMAN e-commerce</h1>
+        <div className="taskLine"></div>
+        <p className="tasktText">Pagina web e-commerce enfocada en la compra y venta de prendas de ropa para un comercio real,Realizada con React js, MongoDB, Mysql y CSS.</p>
+        <button className="btnTask" >Vista en progreso
+        <img className="loadImg" src={loading} alt="" />
+        </button>
+
+      </div>
+      </div>
+
+      <div className="taskDiv">
+         <div className="taskProject">
+        
+        <div className="producto_img">
+        <img className="imgCards" src={enmacipatic} />
+          
+        </div>
+        <h1 className="taskTitle">Red Enmacipatic</h1>
+        <div className="taskLine"></div>
+        <p className="tasktText">App dedicada a el contacto entre personas de la tercera edad, voluntarios y personas del sector, realizada con React js, CSS, y MongoDB</p>
+        <button className="btnTask" >Vista en progreso
+        <img className="loadImg" src={loading} alt="" />
+        </button>
+
+      </div>
+      </div>
+  
     </div>
   );
 }
